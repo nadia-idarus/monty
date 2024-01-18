751890defb3326045d19bccf59e8c473e8b572b5
nadia-idarus <nadia.abdalla@riarauniversity.ac.ke>
2024-01-18 08:56:21 -0800

    stack.c

---
+++ stack.c
@@ -0,0 +1,18 @@
+#include "monty.h"
+
+/**
+ * print_top - Prints the top element of the stack.
+ * @stack_head: Pointer to the head of the stack.
+ * @line_number: Line number in the file.
+ * Return: No return value.
+ */
+void print_top(stack_t **stack_head, unsigned int line_number)
+{
+    // Ignoring unused parameters
+    (void)stack_head;
+    (void)line_number;
+
+    // Reset a specific variable (bus.lifi) to 0
+    bus.lifi = 0;
+}
+
